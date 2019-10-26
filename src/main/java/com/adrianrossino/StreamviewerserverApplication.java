5f6b78ea08fb0d42aeeec63898c895286aabeadd
Adrián Rossino <gurdoman@hotmail.com>
2019-10-26 00:09:06 -0500

    First commit

---
+++ src/main/java/com/adrianrossino/StreamviewerserverApplication.java
@@ -0,0 +1,13 @@
+package com.adrianrossino;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StreamviewerserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StreamviewerserverApplication.class, args);
+	}
+
+}
